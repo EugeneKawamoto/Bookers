37b57dbac028aa23212c9a7d3c74f1c7b75e8fe2
vagrant <vagrant@localhost.localdomain>
2020-08-03 07:41:54 +0000

    add index show create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
   end
 
   def show
-  end
-
-  def new
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
