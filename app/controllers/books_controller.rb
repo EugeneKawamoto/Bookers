a3fad4db42af545832aa7315045d7984dbf9c209
vagrant <vagrant@localhost.localdomain>
2020-08-05 06:41:26 +0000

    add edit success-m

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully updated."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render action: :index
@@ -28,8 +28,10 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    end
   end
 
   def destroy
