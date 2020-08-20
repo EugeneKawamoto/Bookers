9493a47bfde05c008441c5fa36a61f9236a0dc80
川本悠人 <eugenenzv8@gmail.com>
2020-08-20 18:09:20 +0900

    add date_reset

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Bookers
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.paths.add 'lib', eager_load: true  # この行を追加    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
